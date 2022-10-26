68967b043dad0fe472dc1a1ee7df2ab75951b80e
marya1010 <marya_mirmanova@mail.ru>
2022-10-26 11:40:34 +0300

    Update client.py

--- client.py
+++ client.py
@@ -3,9 +3,20 @@ import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect(('127.0.0.1', 55555))
+print(client.recv(1024).decode('utf-8'))
+Port = input().encode('utf-8')
+client.send(Port)
+print(client.recv(1024).decode('utf-8'))
+Host = input().encode('utf-8')
+client.send(Host)
 
+#client.connect((Host, Port))
 while True:
+
     data = client.recv(1024)
     print(data.decode('utf-8'))
-
-    client.send(input().encode('utf-8'))
+    print('**Введите сообщение для сервера:** ')
+    message = input().encode('utf-8')
+    client.send(message)
+    if message == 'exit':
+        break
