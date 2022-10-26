20fa59720cce4ba0fb5c28fd7c3d9e0ebf935f74
marya1010 <marya_mirmanova@mail.ru>
2022-10-26 11:40:37 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,20 +1,36 @@
 import socket
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('127.0.0.1', 55555))
+Host = '127.0.0.1'
+Port = 55555
+server.bind((Host, Port))
 
 server.listen()
+user, address = server.accept()
+user.send('Введите номер порта!'.encode('utf-8'))
+Port = user.recv(1024).decode('utf-8')
+print("Клиент ввел номер порта: ", Port)
+user.send('Введите номер хоста!'.encode('utf-8'))
+Host = user.recv(1024).decode('utf-8')
+print("Клиент ввел номер хоста: ", Host)
+
+#server.bind((Host, Port))
+#server.listen()
+#user, address = server.accept()
+
 #conn, addr = sock.accept()
 # .accept принимает всех кто подключился к серверу
 # видит айпиб тип соединения и тд
-user, address = server.accept()
+
 while True:
 
-    #print('Клиент присоединился')
+    print('**Введите сообщение для клиента:** ')
     #user.send('Вы подключены'.encode('utf-8'))
     user.send(input().encode('utf-8'))
     data = user.recv(1024)
     print(data.decode('utf-8'))
+    if data.decode('utf-8') == 'exit':
+        break
 
 #conn.close()
 
